b2d41fc7f793c92c78ebd6ef964537271e4052b9
vinicius <vinicius.generoso2@etec.sp.gov.br>
2023-10-09 23:11:37 -0300

    NaoConsigoNoLinux

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+int main() {
+    float preco, precoInflacionado;
+
+    
+    printf("Digite o preço do produto: ");
+    scanf("%f", &preco);
+
+    
+    if (preco < 100) {
+        
+        precoInflacionado = preco * 1.10;
+    } else {
+        
+        precoInflacionado = preco * 1.20;
+    }
+
+   
+    printf("Valor original: R$ %.2f\n", preco);
+    printf("Valor inflacionado: R$ %.2f\n", precoInflacionado);
+
+    return 0;
+}
+
